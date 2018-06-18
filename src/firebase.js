6fc8c2880cc8655139da8cad37a1b65af72290c1
Vicente Rotman <vrotmanh@gmail.com>
2018-06-18 15:16:15 -0400

    Setup Firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCvXQsdNo4l8Z76YabUg141wKkyoHvOmg8",
+    authDomain: "linnia-test.firebaseapp.com",
+    databaseURL: "https://linnia-test.firebaseio.com",
+    projectId: "linnia-test",
+    storageBucket: "",
+    messagingSenderId: "21223694064"
+};
+firebase.initializeApp(config);
+export default firebase;
