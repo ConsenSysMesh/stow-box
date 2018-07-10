ec15e82f788036fcc14c7888d320dc292d90e8d5
godfreyhobbs <godfreyhobbs@gmail.com>
2018-07-10 15:49:17 -0700

    remove uglyfy until this is fixed https://github.com/ipfs/js-ipfs-api/issues/611

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -237,19 +237,20 @@ module.exports = {
     // Try to dedupe duplicated modules, if any:
     new webpack.optimize.DedupePlugin(),
     // Minify the code.
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true, // React doesn't support IE8
-        warnings: false
-      },
-      mangle: {
-        screw_ie8: true
-      },
-      output: {
-        comments: false,
-        screw_ie8: true
-      }
-    }),
+    // stopped due to https://github.com/ipfs/js-ipfs-api/issues/611
+    // new webpack.optimize.UglifyJsPlugin({
+    //      compress: {
+    //        screw_ie8: true, // React doesn't support IE8
+    //        warnings: false
+    //      },
+    //      mangle: {
+    //        screw_ie8: true
+    //      },
+    //      output: {
+    //        comments: false,
+    //        screw_ie8: true
+    //      }
+    //    }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin('static/css/[name].[contenthash:8].css'),
     // Generate a manifest file which contains a mapping of all asset filenames
