097bb72af97792711c86b7b9a0f81761b4423a20
Vicente Rotman <vrotmanh@gmail.com>
2018-07-31 12:47:21 -0400

    Fix new encryption

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
     LINNIA_IPFS_HOST: 'ipfs.infura.io',
     LINNIA_IPFS_PORT: '5001',
     LINNIA_IPFS_PROTOCOL: 'https',
-    LINNIA_HUB_ADDRESS: '0xc39f2e4645de2550ee3b64e6dc47f927e8a98934',
+    LINNIA_HUB_ADDRESS: '0x177bf15e7e703f4980b7ef75a58dc4198f0f1172',
     LINNIA_SEARCH_URI: 'http://18.222.147.7:3000'
 };
 
