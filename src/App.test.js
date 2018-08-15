8abcf1e325dec6a8a90e26d08a22c84b47a37005
godfreyhobbs <godfreyhobbs@gmail.com>
2018-08-15 13:29:55 -0700

    adding snapshot tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   // TODO: fix with default props
   ReactDOM.render(<App authenticate={console.log} />, div);
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+     .create(<App authenticate={console.log}/>)
+     .toJSON();
+  expect(tree).toMatchSnapshot();
 });
