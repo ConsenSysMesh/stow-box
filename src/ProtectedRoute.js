c6cb70abbe06cc8b950a3f1e8070e770873c4ac1
MaxBlaushild <blaushmild@gmail.com>
2018-08-21 13:13:55 -0400

    lp435/feat/moreComments

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './layouts/home/Home';
 
+/*
+  This component wraps all of our routes. If this authentication reducer
+  returns success, then it renders the component. If it returns an error
+  in it's state, it shows the AuthError component.
+*/
+
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const { isAuthenticated, authError } = rest;
 
--- src/auth/AuthActions.js
+++ src/auth/AuthActions.js
@@ -29,6 +29,7 @@ const authFailure = authError => ({
   authError,
 });
 
+
 const getWeb3 = () => {
   return new Promise((resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
@@ -44,15 +45,31 @@ const getWeb3 = () => {
   });
 };
 
+/*
+  This action is where the app initializes it's connection to IPFS and the ethereum blockchain.
+  If this action is successful, the user is considered 'loggedIn' and is able to use the app. If it fails,
+  The user needs to take action on her part to log herself in. The AuthError component renders the text
+  of the action that the user needs to take.
+*/
+
 export const authenticate = () => async dispatch => {
   let web3;
 
+  /*
+    First, we check to see that web3 has been injected into the browser window. This will most likely
+    be done by the MetaMask browser extension, but could be another extension or browser.
+  */
   try {
     web3 = await getWeb3();
   } catch (e) {
     console.error(e);
     return dispatch(authFailure(NO_METAMASK));
   }
+
+  /*
+    Next, we make sure that the user has logged into MetaMask. We get the first account address,
+    then check that it exists. It will be undefined if the user's MetaMask is locked.
+  */
 
   const accounts = await web3.eth.getAccounts();
   const address = accounts[0];
@@ -61,6 +78,11 @@ export const authenticate = () => async dispatch => {
     console.error('Metamask is locked!');
     return dispatch(authFailure(LOCKED_METAMASK));
   }
+
+  /*
+    Next, we make sure the IPFS has been properly configured and can be connected to. This is 
+    a developer check rather than a user authentication check.
+  */
 
   const ipfs = new IPFS({ host: host, port: port, protocol: protocol });
 
@@ -71,7 +93,12 @@ export const authenticate = () => async dispatch => {
     return dispatch(authFailure(IPFS_MISCONFIGURED));
   }
 
-  // checking to see if contract exists at address since truffle-contract doesnt expose error
+  /*
+    Finally, we make sure that we are able to connect to the Linnia Smart contracts. We use 
+    Web3 to ping the contract address. If we receive and empty hex, we have provided the wrong
+    hub address. This is another developer check.
+  */
+
   const code = await web3.eth.getCode(hubAddress);
   if (!code || code === '0x0' || code === '0x') {
     console.error('Linnia is not configured correctly!');
@@ -79,6 +106,11 @@ export const authenticate = () => async dispatch => {
   }
 
   const linnia = new Linnia(web3, ipfs, { hubAddress });
+
+  /*
+    Success! Our user is ready to start interacting with the Linnia Protocol. We add the
+    services to our state so that other actions and components can use them.
+  */
 
   dispatch(authSuccess(web3, ipfs, linnia));
 };
